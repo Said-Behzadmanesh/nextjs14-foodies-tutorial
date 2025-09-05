5ec2b2c6a41a3d55d2f71ee57ec59b205bcc9133
Said Behzadmanesh <behmanesh_s@yahoo.com>
2025-09-05 13:35:55 +0200

    new entry with image picker and image upload

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,44 @@
-import React from "react";
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
-export default function MealDetailPage({ params }) {
+export default async function MealDetailPage({ params }) {
+  const meal = await getMeal(params.slug);
+  if (!meal) {
+    notFound();
+  }
+  meal.instructions = (meal.instructions ?? "").replace(/\n/g, "<br />");
+
+  console.log("params: ", params);
+  console.log("meal: ", meal);
+  console.log("params.slug: ", params.slug);
+  console.log("image: ", meal.image);
+  console.log("summary: ", meal.summary);
+  console.log("instructions: ", meal.instructions);
+  console.log("creator: ", meal.creator);
+  console.log("creator_email: ", meal.creator_email);
+
   return (
-    <div>
-      <h1>{params.slug}</h1>
-    </div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>NAME</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 }
